22297e555f06010f9e73810da10db464fb68cc40
CarlAbel <caweb.cph@gmail.com>
2022-04-21 12:07:32 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path")
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "build")
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "build")
+        },
+        port: 3002
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|webp|gif)$/,
+                type: "asset/resource"
+            },
+            {
+                test: /\.js$/i,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+}
