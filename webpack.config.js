d776d7d1f2517a27ab125df27ac8508459d6fa6c
CarlAbel <caweb.cph@gmail.com>
2022-04-22 09:50:37 +0200

    yi

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path")
 module.exports = {
     entry: {
         index: "./src/index.js",
-        subpage: "./src/subpage.js"
+        subpage: "./src/subpage.js",
     },
     output: {
         filename: "[name].bundle.js",
